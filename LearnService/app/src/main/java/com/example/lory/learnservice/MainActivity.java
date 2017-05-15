2274d5cb2f84f40ce5f179455900d192ce1246b8
Lory Hou <lory.hou@augmentum.com>
2017-05-15 23:10:14 +0800

    add bind service

--- LearnService/app/src/main/java/com/example/lory/learnservice/MainActivity.java
+++ LearnService/app/src/main/java/com/example/lory/learnservice/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.lory.learnservice;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,23 +13,40 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements android.content.ServiceConnection {
+    Intent intent;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        intent = new Intent(MainActivity.this, MyService.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         findViewById(R.id.btnStartService).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                startService(new Intent(MainActivity.this, MyService.class));
+                startService(intent);
             }
         });
 
         findViewById(R.id.btnStopService).setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
-                stopService(new Intent(MainActivity.this, MyService.class));
+                stopService(intent);
+            }
+        });
+
+        findViewById(R.id.btnBindService).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                bindService(intent, MainActivity.this, Context.BIND_AUTO_CREATE);
+            }
+        });
+
+        findViewById(R.id.btnUnbindService).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                unbindService(MainActivity.this);
             }
         });
     }
@@ -51,5 +71,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+        System.out.println("service connected");
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName componentName) {
+        System.out.println("");
     }
 }
--- LearnService/app/src/main/java/com/example/lory/learnservice/MyService.java
+++ LearnService/app/src/main/java/com/example/lory/learnservice/MyService.java
@@ -2,6 +2,7 @@ package com.example.lory.learnservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
 
@@ -12,12 +13,14 @@ public class MyService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return new Binder() ;
+
     }
 
     @Override
     public int onStartCommand(Intent intent,int flags, int startId) {
-        new Thread() {
+       /* new Thread() {
             @Override
             public void run() {
                 while(true) {
@@ -30,8 +33,20 @@ public class MyService extends Service {
                 }
 
             }
-        }.start();
+        }.start();*/
 
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        System.out.println("service detroy");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        System.out.println("service create");
     }
 }
